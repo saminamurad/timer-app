ddef50f54ead80414c4ffff966cd4b731aaf3834
saminamurad <sameena.murad@gmail.com>
2023-01-18 12:12:24 +0500

    deploy my timer app to github page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState} from 'react';
+ 
 function App() {
+  let timer= new Date().toLocaleTimeString();
+  const [currentTime, setcurrentTime] = useState(timer);
+  const updateTime =()=>{
+    let timer= new Date().toLocaleTimeString()
+    setcurrentTime(timer)
+
+  }
+setInterval(updateTime,1000)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Current Time Set{timer} </h1>
+
     </div>
   );
 }
